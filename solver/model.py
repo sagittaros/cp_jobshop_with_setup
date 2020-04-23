549a24e9416914bf22c1f82d3b7a3be0aa75158d
Felix <zen9.felix@gmail.com>
2020-04-23 19:43:31 +0800

    First commit

---
+++ solver/model.py
@@ -0,0 +1,278 @@
+import collections
+
+from ortools.sat.python import cp_model
+from datetime import datetime, timedelta
+from .timeline import export_html
+
+# 2 product types P1 and P2
+# 5 machines CP1, CP2, DP1, DP2, PK1 (human)
+# 2 sales items J1 and J2 of the kind P1
+# 2 sales items J3 and J4 of the kind P2
+# time is in hours
+def index():
+    alt = collections.namedtuple(
+        "alt", ["machine_id", "type", "processing_time", "setup_time", "due"]
+    )
+    return [
+        [
+            [alt("CP1", "P1", 3, 1, 10), alt("CP2", "P1", 4, 1, 10)],  # task 1
+            [alt("DP1", "P1", 4, 4, 10), alt("DP2", "P1", 6, 3, 10)],  # task 2
+            [alt("PK1", "P1", 2, 0, 10)],  # task 3
+        ],  # job 1
+        [
+            [alt("CP1", "P1", 3, 1, 21), alt("CP2", "P1", 4, 1, 21)],  # task 1
+            [alt("DP1", "P1", 4, 4, 21), alt("DP2", "P1", 6, 3, 21)],  # task 2
+            [alt("PK1", "P1", 2, 0, 21)],  # task 3
+        ],  # job 2
+        [
+            [alt("CP1", "P2", 3, 1, 22), alt("CP2", "P2", 4, 1, 22)],  # task 1
+            [alt("DP1", "P2", 4, 4, 22), alt("DP2", "P2", 6, 3, 22)],  # task 2
+            [alt("PK1", "P2", 2, 0, 22)],  # task 3
+        ],  # job 3
+        [
+            [alt("CP1", "P2", 3, 1, 16), alt("CP2", "P2", 4, 1, 16)],  # task 1
+            [alt("DP1", "P2", 4, 4, 16), alt("DP2", "P2", 6, 3, 16)],  # task 2
+            [alt("PK1", "P2", 2, 0, 16)],  # task 3
+        ],  # job 4
+    ]
+
+
+def list_machines(jobs):
+    machines_lookup = {}
+    for job in jobs:
+        for task in job:
+            for alt in task:
+                machines_lookup[alt.machine_id] = 1
+    return list(machines_lookup.keys())
+
+
+def compute_horizon(jobs):
+    horizon = 0
+    for job in jobs:
+        for task in job:
+            max_task_duration = 0
+            for alt in task:
+                max_task_duration = max(max_task_duration, alt.processing_time)
+            horizon += max_task_duration
+    return horizon
+
+
+def main():
+    # Model.
+    model = cp_model.CpModel()
+
+    # inputs
+    jobs = index()
+    num_jobs = len(jobs)
+    all_jobs = range(num_jobs)
+    machines = list_machines(jobs)
+
+    # Compute a maximum makespan greedily.
+    horizon = compute_horizon(jobs)
+    print("Horizon = %i" % horizon)
+
+    # Global storage of variables.
+    intervals_per_machines = collections.defaultdict(list)
+    presences_per_machines = collections.defaultdict(list)
+    starts_per_machines = collections.defaultdict(list)
+    ends_per_machines = collections.defaultdict(list)
+    types_per_machines = collections.defaultdict(list)
+    ranks_per_machines = collections.defaultdict(list)
+    job_starts = {}  # indexed by (job_id, task_id).
+    job_presences = {}  # indexed by (job_id, task_id, alt_id).
+    job_ranks = {}  # indexed by (job_id, task_id, alt_id).
+    job_ends = []  # indexed by job_id
+
+    # Populate variables and intervals
+    for job_id in all_jobs:
+        job = jobs[job_id]
+        num_tasks = len(job)
+        previous_end = None
+        for task_id in range(num_tasks):
+            task = job[task_id]
+            if len(task) == 0:
+                # this can happen if no machine can process the job
+                print("no available machines")
+                continue
+
+            min_duration = min([alt.processing_time for alt in task])
+            max_duration = max([alt.processing_time for alt in task])
+
+            # Create main interval for the task.
+            suffix_name = "_j%i_t%i" % (job_id, task_id)
+            start = model.NewIntVar(0, horizon, "start" + suffix_name)
+            duration = model.NewIntVar(
+                min_duration, max_duration, "duration" + suffix_name
+            )
+            end = model.NewIntVar(0, horizon, "end" + suffix_name)
+
+            # Store the start for the solution.
+            job_starts[(job_id, task_id)] = start
+
+            # Add precedence with previous task in the same job.
+            # if previous_end:
+            # model.Add(start >= previous_end)
+            previous_end = end
+
+            # Create alternative intervals.
+            l_presences = []
+            for alt_id, alt in enumerate(task):
+                alt_suffix = "_j%i_t%i_a%i" % (job_id, task_id, alt_id)
+                l_presence = model.NewBoolVar("presence" + alt_suffix)
+                l_start = model.NewIntVar(0, horizon, "start" + alt_suffix)
+                l_duration = alt.processing_time
+                l_end = model.NewIntVar(0, horizon, "end" + alt_suffix)
+                l_interval = model.NewOptionalIntervalVar(
+                    l_start, l_duration, l_end, l_presence, "interval" + alt_suffix
+                )
+                l_rank = model.NewIntVar(-1, num_jobs, "rank" + alt_suffix)
+                l_presences.append(l_presence)
+                l_machine = alt.machine_id
+                l_type = alt.type
+
+                # Link the master variables with the local ones.
+                model.Add(start == l_start).OnlyEnforceIf(l_presence)
+                model.Add(duration == l_duration).OnlyEnforceIf(l_presence)
+                model.Add(end == l_end).OnlyEnforceIf(l_presence)
+
+                # due date constraint
+                model.Add(end < alt.due).OnlyEnforceIf(l_presence)
+
+                # Add the local variables to the right machine.
+                intervals_per_machines[l_machine].append(l_interval)
+                starts_per_machines[l_machine].append(l_start)
+                ends_per_machines[l_machine].append(l_end)
+                presences_per_machines[l_machine].append(l_presence)
+                types_per_machines[l_machine].append(l_type)
+                ranks_per_machines[l_machine].append(l_rank)
+
+                # Store the variables for the solution.
+                job_presences[(job_id, task_id, alt_id)] = l_presence
+                job_ranks[(job_id, task_id, alt_id)] = l_rank
+
+            # Only one machine can process each lot.
+            model.Add(sum(l_presences) == 1)
+        job_ends.append(previous_end)
+
+    # Create machines constraints nonoverlap process
+    for machine_id in machines:
+        intervals = intervals_per_machines[machine_id]
+        if len(intervals) > 1:
+            model.AddNoOverlap(intervals)
+
+    # Transition times and transition costs using a circuit constraint.
+    switch_literals = []
+    for machine_id in machines:
+        machine_starts = starts_per_machines[machine_id]
+        machine_ends = ends_per_machines[machine_id]
+        machine_presences = presences_per_machines[machine_id]
+        machine_types = types_per_machines[machine_id]
+        machine_ranks = ranks_per_machines[machine_id]
+        intervals = intervals_per_machines[machine_id]
+        arcs = []
+        num_machine_tasks = len(machine_starts)
+        all_machine_tasks = range(num_machine_tasks)
+
+        for i in all_machine_tasks:
+            # Initial arc from the dummy node (0) to a task.
+            start_lit = model.NewBoolVar("")
+            arcs.append([0, i + 1, start_lit])
+            # If this task is the first, set both rank and start to 0.
+            model.Add(machine_ranks[i] == 0).OnlyEnforceIf(start_lit)
+            model.Add(machine_starts[i] == 0).OnlyEnforceIf(start_lit)
+            # Final arc from an arc to the dummy node.
+            arcs.append([i + 1, 0, model.NewBoolVar("")])
+            # Self arc if the task is not performed.
+            arcs.append([i + 1, i + 1, machine_presences[i].Not()])
+            model.Add(machine_ranks[i] == -1).OnlyEnforceIf(machine_presences[i].Not())
+
+            for j in all_machine_tasks:
+                if i == j:
+                    continue
+
+                lit = model.NewBoolVar("%i follows %i" % (j, i))
+                arcs.append([i + 1, j + 1, lit])
+                model.AddImplication(lit, machine_presences[i])
+                model.AddImplication(lit, machine_presences[j])
+
+                # Maintain rank incrementally.
+                model.Add(machine_ranks[j] == machine_ranks[i] + 1).OnlyEnforceIf(lit)
+
+                # Compute the transition time if task j is the successor of task i.
+                if machine_types[i] != machine_types[j]:
+                    transition_time = 3
+                    switch_literals.append(lit)
+                else:
+                    transition_time = 0
+                # We add the reified transition to link the literals with the times
+                # of the tasks.
+                model.Add(
+                    machine_starts[j] == machine_ends[i] + transition_time
+                ).OnlyEnforceIf(lit)
+
+        model.AddCircuit(arcs)
+
+    # Objective.
+    makespan = model.NewIntVar(0, horizon, "makespan")
+    model.AddMaxEquality(makespan, job_ends)
+    makespan_weight = 1
+    transition_weight = 5
+    model.Minimize(makespan)
+    # model.Minimize(
+    #     makespan * makespan_weight + sum(switch_literals) * transition_weight
+    # )
+
+    # Write problem to file.
+    with open("problem.proto", "w") as text_file:
+        text_file.write(str(model))
+
+    # Solve.
+    solver = cp_model.CpSolver()
+    solver.parameters.max_time_in_seconds = 60 * 60 * 2
+    status = solver.Solve(model)
+
+    # Print solution.
+    solution = []
+    if status == cp_model.FEASIBLE or status == cp_model.OPTIMAL:
+        for job_id in all_jobs:
+            for task_id in range(len(jobs[job_id])):
+                start_value = solver.Value(job_starts[(job_id, task_id)])
+                machine = 0
+                product_type = ""
+                duration = 0
+                select = 0
+                rank = -1
+
+                for alt_id in range(len(jobs[job_id][task_id])):
+                    if solver.BooleanValue(job_presences[(job_id, task_id, alt_id)]):
+                        duration = jobs[job_id][task_id][alt_id].processing_time
+                        machine = jobs[job_id][task_id][alt_id].machine_id
+                        product_type = jobs[job_id][task_id][alt_id].type
+                        select = alt_id
+                        rank = solver.Value(job_ranks[(job_id, task_id, alt_id)])
+
+                end_value = start_value + duration
+                solution.append(
+                    {
+                        "machine_id": machine,
+                        "label": ("j%i: %s" % (job_id, product_type)),
+                        "start": datetime.today() + timedelta(days=start_value),
+                        "end": datetime.today() + timedelta(days=end_value),
+                    }
+                )
+
+                print(
+                    "  Job %i starts at %i (alt %i, duration %i) with rank %i on machine %s"
+                    % (job_id, start_value, select, duration, rank, machine)
+                )
+
+        print("Solve status: %s" % solver.StatusName(status))
+        print("Objective value: %i" % solver.ObjectiveValue())
+        print("Makespan: %i" % solver.Value(makespan))
+        export_html(solution)
+    elif status == cp_model.INFEASIBLE:
+        print("INFEASIBLE")
+
+
+if __name__ == "__main__":
+    main()
---
+++ solver/timeline.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+from datetime import date
+import gviz_api
+
+page_template = """
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['timeline']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var container = document.getElementById('timeline');
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable(%s);
+        chart.draw(dataTable);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="timeline" style="height: %ipx;"></div>
+  </body>
+</html>
+"""
+
+
+def export_html(data, height: int = 300):
+    schema = {
+        "machine_id": ("string", "Machine"),
+        "label": ("string", "Label"),
+        "start": ("date", "Start"),
+        "end": ("date", "End"),
+    }
+
+    data_table = gviz_api.DataTable(schema)
+    data_table.LoadData(data)
+
+    # Create a JSON string.
+    json = data_table.ToJSon(columns_order=("machine_id", "label", "start", "end"))
+
+    with open("timeline.html", "w") as html:
+        html.write(page_template % (json, height))
+
+
+def main():
+    # Creating the data
+    data = [
+        {
+            "machine_id": "M1",
+            "label": "label 1",
+            "start": date(2009, 11, 2),
+            "end": date(2011, 1, 2),
+        },
+        {
+            "machine_id": "M2",
+            "label": "label 2",
+            "start": date(2009, 3, 1),
+            "end": date(2012, 4, 2),
+        },
+        {
+            "machine_id": "M3",
+            "label": "label 3",
+            "start": date(2012, 3, 1),
+            "end": date(2012, 4, 2),
+        },
+    ]
+    export_html(data)
+
+
+if __name__ == "__main__":
+    main()
